fdb25de0f3af53430f391fa675561779a26f722b
Higino Sergio Marinho da Silva <hsilva@NB325.lan>
2020-11-12 19:50:11 +0000

    update configuration

--- expense-service/src/main/resources/a-schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists category CASCADE; 
-drop table if exists expense CASCADE;
-drop table if exists user CASCADE;
-create table category (id bigint not null, description varchar(255), name varchar(255), primary key (id));
-create table expense (id bigint not null, description varchar(255), expense_date timestamp, location varchar(255), category_id bigint, user_id bigint, primary key (id));
-create table user (id bigint not null, email varchar(255), name varchar(255), primary key (id));
-alter table expense add constraint FKmvjm59reb5i075vu38bwcaqj6 foreign key (category_id) references category;
-alter table expense add constraint FK758h5dgdblrpwoaaycbmn29i0 foreign key (user_id) references user;
